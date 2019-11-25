2a698ef7609e5198823d2c684eed0dd9d9ed1e56
tsubasaozawa <tsubasaozawa@users.noreply.github.com>
2019-11-25 19:46:31 +0900

    payjpでの商品購入実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_24_120052) do
+ActiveRecord::Schema.define(version: 2019_11_25_093518) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "customer_id"
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(version: 2019_11_24_120052) do
     t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
+  create_table "investments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "investment_amount", null: false
+    t.bigint "user_id"
+    t.bigint "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_investments_on_project_id"
+    t.index ["user_id"], name: "index_investments_on_user_id"
+  end
+
   create_table "projects", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
     t.text "content", null: false
@@ -28,6 +38,8 @@ ActiveRecord::Schema.define(version: 2019_11_24_120052) do
     t.date "limit", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -48,4 +60,6 @@ ActiveRecord::Schema.define(version: 2019_11_24_120052) do
   end
 
   add_foreign_key "cards", "users"
+  add_foreign_key "investments", "projects"
+  add_foreign_key "investments", "users"
 end
