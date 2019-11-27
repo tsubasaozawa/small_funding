17c9a57172e73bece07ae0d2edb354f8953e613d
tsubasaozawa <tsubasaozawa@users.noreply.github.com>
2019-11-28 01:34:43 +0900

    お気に入りタグ機能実装

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,10 @@
 class Category < ApplicationRecord
   has_many :project_categories,    dependent: :destroy
   has_many :projects,              through: :project_categories
+
+  has_many :user_categories,       dependent: :destroy
+  has_many :users,                 through: :user_categories
+
   validates :tag_name,          presence: true, uniqueness: true
 
 #  # タグのバリデーションは後日実装
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,34 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :projects, dependent: :destroy
   has_many :investments, dependent: :destroy
 
   has_many :likes, dependent: :destroy
   has_many :liked_projects, through: :likes, source: :project
+
+  has_many :user_categories, dependent: :destroy
+  has_many :categories, through: :user_categories
   
   validates :nickname, presence: true, length: { maximum: 10 }
 
   def already_liked?(project)
     self.likes.exists?(project_id: project.id)
+  end
+
+  def save_categories(tags)
+    current_tags = self.categories.pluck(:tag_name) unless self.categories.nil?
+    old_tags = current_tags - tags
+    new_tags = tags - current_tags
+
+    old_tags.each do |old_name|
+      self.categories.delete Category.find_by(tag_name: old_name)
+    end
+
+    new_tags.each do |new_name|
+      post_category = Category.find_or_create_by(tag_name: new_name)
+      self.categories << post_category
+    end
   end
 end
